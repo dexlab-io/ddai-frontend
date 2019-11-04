ecbf5eccd2157de3558166829e20fdfefbb14f1a
alexintosh <alessio.d@gmail.com>
2019-11-04 09:28:09 -0500

    Adding db via airtable.

--- src/components/NotificationsDrawer.js
+++ src/components/NotificationsDrawer.js
@@ -1,12 +1,12 @@
 import React from "react";
 import NotificationCard from './NotificationCard';
+import NotificationDB from './NotificationDb';
 import Drawer from '@material-ui/core/Drawer';
 import { Context } from '../context';
 import styled from "styled-components";
 
 const NoTransactionsText = styled.div`
     padding: 2%
-    
 `;
 
 
@@ -14,10 +14,13 @@ class NotificationsDrawer extends React.Component {
     render() {
         return(
             <Drawer anchor="right" open={this.context.notificationDrawerOpen} onClose={this.context.closeNotificationsDrawer}>
-                {this.context.transactions.length == 0 && <NoTransactionsText> No transactions to track yet </NoTransactionsText>}
-                {this.context.transactions.map((tx, index) => (
-                    <NotificationCard key={index} tx={tx} />
+                {this.context.logs == 0 && <NoTransactionsText> No transactions to track yet </NoTransactionsText>}
+                {this.context.logs.map((log, index) => (
+                    <NotificationDB key={index} log={log} />
                 ))}
+                {/* {this.context.transactions.map((tx, index) => (
+                    <NotificationCard key={index} tx={tx} />
+                ))} */}
             </Drawer>
         )
     }
